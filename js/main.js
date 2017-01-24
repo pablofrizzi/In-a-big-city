001c1e7f83fc6e9df0f93320f4cc7fadf1f686d7
pablofrizzi <pablofrizzi@gmail.com>
2017-01-24 12:36:36 +0100

    main.js changed

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-var $btn = document.getElementById('btn');
-var $number = document.getElementById('number').textContent;
-var currCount = parseInt($number,10);
-var $clicked = document.getElementById('appreciate');
+var btn = document.getElementById('btn');
+var clicked = document.getElementById('appreciate');
 
 var appCount = function() {
-  currCount += 1;
-  document.getElementById('number').textContent = currCount;
-  $btn.removeEventListener('click', appCount);
-  $btn.textContent = "THANK YOU!";
-  $clicked.className = 'clicked';
-  $btn.disabled;
+  var number = document.getElementById('number').textContent;
+  document.getElementById('number').textContent = parseInt(number,10) + 1;
+  /*button remove*/
+  btn.removeEventListener('click', appCount);
+  btn.textContent = "THANK YOU!";
+  clicked.className = 'clicked';
+  btn.disabled;
 }
 
-$btn.addEventListener('click', appCount);
+btn.addEventListener('click', appCount);
