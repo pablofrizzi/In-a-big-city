9dd32b9541ae1e69f695f46992d8b4ae89b423f5
pablofrizzi <pablofrizzi@gmail.com>
2017-01-24 06:44:53 +0100

    app button fixed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 var $btn = document.getElementById('btn');
 var $number = document.getElementById('number').textContent;
 var currCount = parseInt($number,10);
-
+var $clicked = document.getElementById('appreciate');
 
 var appCount = function() {
   currCount += 1;
   document.getElementById('number').textContent = currCount;
   $btn.removeEventListener('click', appCount);
+  $btn.textContent = "THANK YOU!";
+  $clicked.className = 'clicked';
+  $btn.disabled;
 }
 
 $btn.addEventListener('click', appCount);
