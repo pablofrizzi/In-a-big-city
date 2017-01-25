43f90309ccb19154a1ee8fdc3edd7b3b33a5fb25
pablofrizzi <pablofrizzi@gmail.com>
2017-01-25 22:14:06 +0100

    main.js changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 var btn = document.getElementById('btn');
+var number = document.getElementById('number');
 var clicked = document.getElementById('appreciate');
 
 var appCount = function() {
-  var number = document.getElementById('number').textContent;
-  document.getElementById('number').textContent = parseInt(number,10) + 1;
-  /*button remove*/
+  number.textContent = parseInt(number.textContent, 10) + 1;
   btn.removeEventListener('click', appCount);
   btn.textContent = "THANK YOU!";
   clicked.className = 'clicked';
